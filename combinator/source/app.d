4d955a6dae3cbaab70376902c157ebf5c848f926
Nafees Hassan <nafeeshassan16@gmail.com>
2024-01-14 02:46:42 +0500

    Implemented rater interface and ClashMap

--- combinator/source/app.d
+++ combinator/source/app.d
@@ -16,3 +16,26 @@ void main(){
 		classes ~= c;
 	}
 }
+
+struct ClashMap{
+	void[0][string][string] set;
+	alias set this;
+	pragma(inline, true) void add(
+			string aName, string aSec, string bName, string bSec){
+		set[aName ~ '-' ~ aSec][bName ~ '-' ~ bSec] = (void[0]).init;
+	}
+	pragma(inline, true) bool clashes(
+			string aName, string aSec, string bName, string bSec){
+		return (bName ~ '-' ~ bSec) !in set[aName ~ '-' ~ aSec];
+	}
+
+	this(Class[] classes){
+		foreach (i, a; classes){
+			foreach (j, b; classes){
+				if (i == j) continue;
+				if (a.overlaps(b))
+					add(a.name, a.section, b.name, b.section);
+			}
+		}
+	}
+}
--- combinator/source/rater.d
+++ combinator/source/rater.d
@@ -7,46 +7,77 @@ import common;
 /// Rates consistency of a timetable.
 /// Params: `classes` is sorted classes of timetable, by days
 /// Returns: rating (lower is better), range is 0 to 1000
-int rateConsistency(Class[][DayOfWeek] classes){
+uint rateConsistency(Class[][DayOfWeek] classes){
 	if (classes.length == 0)
 		return 0; // best timetable lol
 	TimeOfDay prevStart = classes[classes.keys[0]][0].time,
 						prevEnd = classes[classes.keys[0]][$ - 1].time +
 							classes[classes.keys[0]][$ - 1].duration;
 	classes.remove(classes.keys[0]);
-	long diff = 0;
+	size_t diff = 0;
 	foreach (day, sessions; classes){
 		TimeOfDay start = sessions[0].time,
 							end = sessions[$ - 1].time + sessions[$ - 1].duration;
-		diff += ((start - prevStart) + (end - prevEnd)).total!"minutes";
+		diff += ((start > prevStart ? start - prevStart : prevStart - start)
+				+ (end > prevEnd ? end - prevEnd : prevEnd - end)).total!"minutes";
 		prevStart = start;
 		prevEnd = end;
 	}
-	return cast(int)(diff * 1000 / (7 * 24 * 60));
+	return cast(uint)(diff * 1000 / (7 * 24 * 60));
 }
 
 /// Rates how many days timetable spans
 /// Params: `classes` is classes of timetable, in any order
 /// Returns: rating (lower is better), range is 0 to 1000
-int rateDays(Class[] classes){
+uint rateDays(Class[] classes){
 	ubyte flags;
-	int count;
+	ubyte count;
 	foreach (session; classes){
 		count += !(flags & (1 << session.day));
 		flags |= 1 << session.day;
 	}
-	return count;
+	return count * 1000 / 7;
 }
 
 /// Rates how many gaps there are in timetable
 /// Params: `classes` is sorted classes of timetable, by days
 /// Returns: rating (lower is better), range is 0 to 1000
-int rateGaps(Class[][DayOfWeek] classes){
-	long gaps;
+uint rateGaps(Class[][DayOfWeek] classes){
+	size_t gaps;
 	foreach (day, sessions; classes){
-		foreach (session; sessions){
-// TODO continue from here
+		if (sessions.length == 0)
+			continue;
+		foreach (i, curr; sessions[1 .. $]){
+			Class prev = sessions[i - 1];
+			gaps += (curr.time - (prev.time + prev.duration)).total!"minutes";
 		}
 	}
-	return cast(int)(gaps * 1000 / (7 * 24 * 60));
+	return cast(uint)(gaps * 1000 / (7 * 24 * 60));
+}
+
+/// Rates a timetable based on consistency, days, and gaps
+/// Params:
+/// * `classes` is the timetable. it will be mutated (sorted)
+/// * weights for rating methods
+/// Returns: rating, 0 to 1000. Lower is better
+uint rate(Class[] classes, uint consistency = 1, uint days = 1, uint gaps = 1){
+	int ret;
+	classesSortByTime(classes);
+	Class[][DayOfWeek] byDays;
+
+	if (consistency && gaps){
+		foreach (session; classes){
+			if (session.day !in byDays)
+				byDays[session.day] = [session];
+			else
+				byDays[session.day] ~= session;
+		}
+
+		if (consistency)
+			ret += consistency * rateConsistency(byDays);
+		if (gaps)
+			ret += gaps * rateGaps(byDays);
+	}
+	ret += days * rateDays(classes);
+	return ret / (consistency + days + gaps);
 }
