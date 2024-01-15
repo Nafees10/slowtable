900710155a56ce8dcd956ea9b295068b4d72af86
Nafees Hassan <nafeeshassan16@gmail.com>
2024-01-15 08:26:20 +0500

    Added install script to make symlinks

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+ln -s $PWD/bin/stparse  $HOME/.local/bin/stparse
+ln -s $PWD/bin/stfilter $HOME/.local/bin/stfilter
+ln -s $PWD/bin/sthtml   $HOME/.local/bin/sthtml
+ln -s $PWD/bin/stcomb   $HOME/.local/bin/stcomb
