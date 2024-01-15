745a918f5845dd8c7053fa65435915c51a34729c
Nafees Hassan <nafeeshassan16@gmail.com>
2024-01-15 06:57:28 +0500

    Implemented genComb

--- combinator/source/app.d
+++ combinator/source/app.d
@@ -1,10 +1,16 @@
 import std.stdio,
-			 std.string;
+			 std.array,
+			 std.string,
+			 std.algorithm;
 
 import common;
 
+/// maximum number of courses this will let you generate combinations for
+enum COURSES_LIMIT = 15; // what insane person wants this much?
+
 void main(){
 	Class[] classes;
+	string[string] nameSec;
 	while (!stdin.eof){
 		string line = readln.chomp("\n");
 		Class c;
@@ -14,7 +20,43 @@ void main(){
 			continue;
 		}
 		classes ~= c;
+		if (c.name in nameSec ||
+				nameSec[c.name].canFind(c.section))
+			continue;
+		nameSec[c.name] ~= c.section;
 	}
+}
+
+/// Generate all combinations.
+Class[][] genComb(Class[] classes, string[string] nameSec){
+	ClashMap clashes = ClashMap(classes);
+	if (nameSec.length > COURSES_LIMIT)
+		throw new Exception(
+				"Refusing to generate combinations for that many courses");
+	if (nameSec.keys.length){
+		return genComb(classes, nameSec, clashes, 0);
+	}
+	return null;
+}
+
+Class[][] genComb(Class[] classes, string[string] nameSec,
+		ClashMap clashes, uint index){
+	static Set!string picks;
+	immutable bool isLeaf = index + 1 == nameSec.keys.length;
+	immutable string course = nameSec.keys[index];
+	Class[][] ret;
+	foreach (sec; nameSec[course]){
+		immutable string selection = course ~ '-' ~ sec;
+		if (clashes.clashes(picks, selection))
+			continue;
+		picks.add(selection);
+		if (isLeaf)
+			ret ~= subset(classes, picks);
+		else
+			ret ~= genComb(classes, nameSec, clashes, index + 1);
+		picks.remove(selection);
+	}
+	return ret;
 }
 
 /// A Set of type `T`
@@ -30,6 +72,11 @@ struct Set(T){
 	void remove(T val){
 		set.remove(val);
 	}
+}
+
+/// Returns: a subset of timetable, containing only set classes sections
+Class[] subset(Class[] classes, Set!string set){
+	return classes.filter!(c => set.exists(c.name ~ '-' ~ c.section)).array;
 }
 
 /// Stores overlap info about classes
@@ -59,7 +106,10 @@ struct ClashMap{
 
 	/// Returns: whether a course will clash with other picked courses
 	bool clashes(Set!string picks, string name, string sec){
-		immutable string cmp = name ~ '-' ~ sec;
+		return clashes(picks, name ~ '-' ~ sec);
+	}
+	/// ditto
+	bool clashes(Set!string picks, string cmp){
 		if (cmp !in sets)
 			return false;
 		Set!string clashSet = sets[cmp];
