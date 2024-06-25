66e6e925b8e7498de23527ef60921c5b2677969f
Nafees Hassan <nafeeshassan16@gmail.com>
2024-06-25 14:51:50 +0500

    Added stdelab package

--- link.sh
+++ link.sh
@@ -2,4 +2,5 @@
 ln -s $PWD/bin/stparse  $HOME/.local/bin/stparse
 ln -s $PWD/bin/stfilter $HOME/.local/bin/stfilter
 ln -s $PWD/bin/sthtml   $HOME/.local/bin/sthtml
+ln -s $PWD/bin/stdelab  $HOME/.local/bin/stdelab
 ln -s $PWD/bin/stcomb   $HOME/.local/bin/stcomb
---
+++ stdelab/source/app.d
@@ -0,0 +1,36 @@
+import std.stdio,
+			 std.string;
+
+import common;
+
+void main(){
+	while (!stdin.eof){
+		string line = readln.chomp("\n");
+		if (line.length == 0) continue;
+		writeln(line); // print name back
+
+		while (!stdin.eof){
+			line = readln.chomp("\n");
+			if (line == "over")
+				break;
+			Class c;
+			try{
+				c = Class.deserialize(line);
+			} catch (Exception){
+				continue;
+			}
+			if (c.name.length >= 3 && c.name[$ - 3 .. $] == "lab"){
+				c.name.length -= 3;
+				ptrdiff_t index = c.section.indexOf(",");
+				if (index > 0)
+					c.section = c.section[0 .. index];
+				index = cast(ptrdiff_t)c.section.length - 1;
+				while (index > 0 && c.section[index .. $].isNumeric)
+					index --;
+				c.section = c.section[0 .. index + 1];
+			}
+			c.serialize.writeln;
+		}
+		writeln("over");
+	}
+}
